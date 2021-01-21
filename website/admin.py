69d6b013dd10cb80a5d55de9105996eb60daab4c
uadson <uadsoncastro@gmail.com>
2021-01-21 17:52:49 -0300

    create models

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Categoria, Livro
 
 # Register your models here.
+
+
+class LivroAdmin(admin.ModelAdmin):
+    list_per_page = 10
+
+    list_display = (
+        'titulo',
+        'autor',
+        'isbn',
+        'ano_pub',
+        'categoria',
+    )
+
+    list_display_links = (
+        'titulo',
+    )
+
+    
+admin.site.register(Livro, LivroAdmin)
+admin.site.register(Categoria)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__ (self):
+        return self.nome
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200, null=False, blank=False)
+    autor = models.CharField(max_length=200, null=False, blank=False)
+    ano_pub = models.DateField(default=0, null=False, blank=False)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    isbn = models.CharField(max_length=50, null=False, blank=False)
+    descricao = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.titulo
