28a3d8dc242f104d36648b88121bccc6194d1fb4
uadson <uadsoncastro@gmail.com>
2021-01-22 11:32:35 -0300

    added pagination

--- website/urls.py
+++ website/urls.py
@@ -6,5 +6,9 @@ app_name = 'website'
 
 urlpatterns = [
 	# ex.: index/
-	path('', views.index, name='index')
+	path('', views.index, name='index'),
+	# ex.: index/4
+	path('<int:livro_id>', views.detail, name='detail'),
+	# ex.: index/search/?term=?
+	path('search/', views.search, name='search'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Livro, Categoria
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 def index(request):
-	return render(request, 'website/index.html')
+	livros = Livro.objects.order_by('-id')
+
+	paginator = Paginator(livros, 5)
+
+	page = request.GET.get('page')
+	livros = paginator.get_page(page)
+
+	return render(request, 'website/index.html', {
+		'livros': livros
+	})
+
+def detail(request, livro_id):
+
+	livro = get_object_or_404(Livro, id=livro_id)
+
+	context = {
+		'livro': livro
+	}
+	return render(request, 'website/detail.html', context)
+
+def search(request):
+	term = request.GET.get('term')	
+
+	livros = Livro.objects.order_by('-id').filter(
+		titulo=term,
+	)
+
+	context = {
+		'livros': livros
+	}
+
+	paginator = Paginator(livros, 5)
+
+	page = request.GET.get('page')
+	livros = paginator.get_page(page)
+
+	return render(request, 'website/search.html', context)
