aa051c3379a623bc889a1efd51a631cde5ae2a12
uadson <uadsoncastro@gmail.com>
2021-01-29 11:42:09 -0300

    added login screen

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'accounts'
+
+urlpatterns = [
+	path('', views.login, name='index_login'),
+	path('login/', views.login, name='login'),
+	path('logout/', views.logout, name='logout'),
+	path('register/', views.register, name='register'),
+	path('dashboard/', views.dashboard, name='dashboard'),
+]
--- webapp/urls.py
+++ webapp/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('accounts/', include('accounts.urls')),
     path('index/', include('website.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
