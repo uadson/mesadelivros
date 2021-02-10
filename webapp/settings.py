06ca132b7937d38b03c02d6289a6658be8aec55d
uadson <uadsoncastro@gmail.com>
2021-02-10 09:33:25 -0300

    commit

--- webapp/settings.py
+++ webapp/settings.py
@@ -83,8 +83,10 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR}/db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
