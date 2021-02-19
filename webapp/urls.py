8f6bd589ccc89c1058858876725a4e3d3f7f61d3
uadson <uadsoncastro@gmail.com>
2021-02-19 08:35:44 -0300

    altered urls

--- webapp/urls.py
+++ webapp/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('index/', include('website.urls')),
+    path('', include('website.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
