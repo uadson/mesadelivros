9dbc046e83751fdd2892e69b6f4f1b99edff128f
uadson <uadsoncastro@gmail.com>
2021-08-31 13:18:16 -0300

    Criado CDN para servir static files

--- webapp/settings/base.py
+++ webapp/settings/base.py
@@ -42,8 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
     'website',
     'accounts',
+
+    # 3rd apps
+    'storages',
 ]
 
 MIDDLEWARE = [
